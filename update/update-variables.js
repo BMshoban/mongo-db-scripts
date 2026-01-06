8ce4be05276b3fae7a0f68399d3c80d8dafe3cfa
shobanb1 <shobanb@maxyfi.com>
2026-01-06 15:31:26 +0530

    email filter

--- update/update-variables.js
+++ update/update-variables.js
@@ -1,11 +1,11 @@
 // ===== Impact Preview =====
 db.users.countDocuments({
-  email: FILTER_EMAIL
+  email: "FILTER_EMAIL"
 });
 
 // ===== UPDATE OPERATION =====
 db.users.updateOne(
-  { email: FILTER_EMAIL },
+  { email: "FILTER_EMAIL" },
   {
     $set: SET_DATA
   }
