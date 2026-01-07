a90664f68eb39528bf64831d73343d24e479ae25
shobanb1 <shobanb@maxyfi.com>
2026-01-07 12:18:14 +0530

    added new  payment-1 variable

--- update/newone.js
+++ update/newone.js
@@ -4,22 +4,24 @@ db.customers.countDocuments({
   "payments._id": { $in: VAR3 }
 });
 
+// ===== Update Payments with Different Amounts =====
 db.customers.updateMany(
   {
     org_id: "VAR1",
-    _id: { $in: VAR2 },
-    "payments._id": { $in: VAR3 }
+    _id: { $in: VAR2 }
   },
   {
     $set: {
-      "payments.$[p].amount": VAR4,
-      updatedBy: "VAR5",
+      "payments.$[p1].amount": VAR4,
+      "payments.$[p2].amount": VAR5,
+      updatedBy: "VAR6",
       updatedAt: new Date()
     }
   },
   {
     arrayFilters: [
-      { "p._id": { $in: VAR3 } }
+      { "p1._id": ObjectId("PAYMENT_ID_1") },
+      { "p2._id": ObjectId("PAYMENT_ID_2") }
     ]
   }
 );
