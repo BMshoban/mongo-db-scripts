b1a40f720056e4f116ad447e3030dcb7878be1f7
devopss projects practice <shoban200327@gmail.com>
2026-06-23 12:13:42 +0530

    Refactor backup and count functions for customer data

--- update/newone.js
+++ update/newone.js
@@ -1,27 +1,31 @@
 //BACKUP_REQUIRED=true
 if (typeof DRY_RUN === "undefined") DRY_RUN = true;
 
-const collection = "organizations2_locals";
+const cus_collection = "customerdata1";
 
+const ORG_IDS = [
+  "6394640c117bb38fdd8372e5",
+  "6394640c117bb38fdd8372e5"
+];
 async function backupCollections() {
   return [
     {
-      collection: "organizations2_locals",
+      collection: "customerdata1",
       filter: {
-        _id: VAR1
+        org_id: { $in: ORG_IDS }
       }
     }
   ];
 }
 async function count() {
-  const org_count = await db[collection].countDocuments({
-    _id: VAR1,
+  const cus_cont_count = await db[cus_collection].countDocuments({
+    org_id: { $in: ORG_IDS }
   });
 
-  print(`MATCHED:${collection}=${org_count}`);
-  print(`TOTAL_MATCHED=${org_count}`);
+  print(`MATCHED:cus_cont_count=${cus_cont_count}`);
+  print(`TOTAL_MATCHED=${cus_cont_count}`);
 
-  return { org_count };
+  return { cus_cont_count };
 }
 
 async function script() {
@@ -30,16 +34,130 @@ async function script() {
     return;
   }
 
-  print("Executing updates...");
+print("Executing updates...");
 
-  const org_update = await db[collection].updateMany(
-    { _id: VAR1 },
-    { $set: { en_sort: true } }
-  );
+ const cus_cont_update_1 = await db[cus_collection].updateMany(
+  { org_id: { $in: ORG_IDS } },
+  [
+    {
+      $set: {
+        days_since_created: {
+          $cond: [
+            { $and: ["$dspd", { $ne: ["$dspd", 0] }] },
+            {
+              $dateDiff: {
+                startDate: {
+                  $dateFromString: {
+                    dateString: {
+                      $dateToString: {
+                        date: { $toDate: "$dspd" },
+                        format: "%Y-%m-%d",
+                        timezone: "CST6CDT",
+                      },
+                    },
+                    format: "%Y-%m-%d",
+                    timezone: "CST6CDT",
+                  },
+                },
+                timezone: "CST6CDT",
+                endDate: "$$NOW",
+                unit: "day",
+              },
+            },
+            0,
+          ],
+        },
+        dspr: {
+          $cond: [
+            {
+              $and: [
+                "$dsprd",
+                { $ne: ["$dsprd", 0] },
+                { $gt: ["$dsprd", 10000] },
+              ],
+            },
+            {
+              $dateDiff: {
+                startDate: {
+                  $dateFromString: {
+                    dateString: {
+                      $dateToString: {
+                        date: { $toDate: "$dsprd" },
+                        format: "%Y-%m-%d",
+                        timezone: "CST6CDT",
+                      },
+                    },
+                    format: "%Y-%m-%d",
+                    timezone: "CST6CDT",
+                  },
+                },
+                timezone: "CST6CDT",
+                endDate: "$$NOW",
+                unit: "day",
+              },
+            },
+            0,
+          ],
+        },
+      },
+    },
+  ],
+  { multi: true }
+);
 
-  print(`UPDATED: ${collection}=${org_update.modifiedCount}`);
+const cus_cont_update_2 = await db[cus_collection].updateMany(
+      { org_id: { $in: ORG_IDS },ipgm: true },
+  [
+    {
+      $set: {
+        "pgbg.dspr": {
+          $cond: [
+            {
+              $and: [
+                "$pgbg.dsprd",
+                { $ne: ["$pgbg.dsprd", 0] },
+                { $gt: ["$pgbg.dsprd", 10000] },
+              ],
+            },
+            {
+              $dateDiff: {
+                startDate: {
+                  $dateFromString: {
+                    dateString: {
+                      $dateToString: {
+                        date: { $toDate: "$pgbg.dsprd" },
+                        format: "%Y-%m-%d",
+                        timezone: "CST6CDT",
+                      },
+                    },
+                    format: "%Y-%m-%d",
+                    timezone: "CST6CDT",
+                  },
+                },
+                timezone: "CST6CDT",
+                endDate: "$$NOW",
+                unit: "day",
+              },
+            },
+            0,
+          ],
+        },
+      },
+    },
+  ],
+  { multi: true }
+);
+print(`UPDATED_1:${cus_collection}=${cus_cont_update_1.modifiedCount}`);
 
-  print("acknowledged: true");
+print(`UPDATED_2:${cus_collection}=${cus_cont_update_2.modifiedCount}`);
+print(
+  `UPDATED:${cus_collection}=${
+    cus_cont_update_1.modifiedCount +
+    cus_cont_update_2.modifiedCount
+  }`
+);
+
+print("acknowledged: true");
 }
 
 (async function main() {
